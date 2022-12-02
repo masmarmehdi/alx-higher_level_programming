c61a76dbc7b9f222d7bd9ad23fb506f132c7e60a
Mehdi Masmar <masmar507@gmail.com>
2022-12-01 16:05:45 -0800

    args + if statement as required

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-import sys
-if len(sys.argv) > 1:
-    print("{} arguments:".format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
-else:
-    print("0 arguments.")
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 1:
+        if len(sys.argv) == 2:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
+    else:
+        print("0 arguments.")
