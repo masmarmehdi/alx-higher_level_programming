42398daa0494095769c11e0112594a7176858316
Mehdi Masmar <masmar507@gmail.com>
2022-12-05 14:47:49 -0800

    quick fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,8 @@
 def print_matrix_integer(matrix=[[]]):
     for line in matrix:
         for element in line:
-            print(element, end = ' ')
+            if element != row[-1]:
+                print("{:d}".format(element), end="")
+            else:
+                print("{:d}".format(element), end="")
         print()
