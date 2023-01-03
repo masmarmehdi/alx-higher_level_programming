df6d0f14ae8db2018222df22abe8355d22b5d4db
Mehdi Masmar <masmar507@gmail.com>
2023-01-03 07:51:25 -0800

    pycodestyle fix

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -4,7 +4,7 @@
 
 
 class Rectangle:
-    """ Rectangle class 
+    """Rectangle class
 
     Attribute:
         number_of_instances (int): The number of rectangle instances
@@ -12,8 +12,10 @@ class Rectangle:
     """
     number_of_instances = 0
     print_symbol = "#"
+
     def __init__(self, width=0, height=0):
         """Initialize a new rectangle.
+
         Args:
             width (int): the width of the rectangle
             height (int): the height of the rectangle
