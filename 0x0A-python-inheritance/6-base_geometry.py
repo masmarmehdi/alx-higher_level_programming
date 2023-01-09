91aef6f1da2edd391369034a3785bb03fec7e7d8
Mehdi Masmar <masmar507@gmail.com>
2023-01-09 15:02:30 -0800

    adding area method: not implemented yet

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a BaseGeometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+    def area(self):
+        raise Exception("area() is not implemented")
