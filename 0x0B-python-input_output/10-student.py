d8b6f04e00b4d7a1641d455b4c9740694d69f9bf
Mehdi Masmar <masmar507@gmail.com>
2023-01-10 06:18:32 -0800

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a student class"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialization of a new student
+
+        Args:
+            first_name (str): first name of the student
+            last_name (str): last name of the student
+            age (int): age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Converting to JSON
+
+        Args:
+            attrs (list): The attributes to represent if provided
+        """
+        if type(attrs) == list:
+            if all(type(element) == str for element in attrs):
+                return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict__
