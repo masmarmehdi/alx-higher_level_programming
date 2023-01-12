6e83bc8093b566a90e8bb92af87700d4514a8d92
Mehdi Masmar <masmar507@gmail.com>
2023-01-12 01:10:35 -0800

    Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -30,6 +30,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Set the current width of the rectangle"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -40,6 +44,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Set the current height of the rectangle"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -50,6 +58,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Set the current x coordinate of the rectangle"""
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -60,4 +72,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Set the current y coordinate of the rectangle"""
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
