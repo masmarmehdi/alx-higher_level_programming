c33b061127c6d84a7abbbc5801f11ca807f28ea8
Mehdi Masmar <masmar507@gmail.com>
2023-01-12 04:25:42 -0800

    Rectangle to dictionary

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -143,3 +143,13 @@ class Rectangle(Base):
                     self.x = value
                 elif key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        """Rectangle instance to dictionary represntation"""
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
