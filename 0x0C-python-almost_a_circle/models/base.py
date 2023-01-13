8027d5f1afdd7e5d34c624be79da29443d5bbf73
Mehdi Masmar <masmar507@gmail.com>
2023-01-13 02:06:20 -0800

    JSON string to dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,7 +28,12 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """"""
+        """
+        Save to file
+
+        Args:
+            list_objs (list): A list of inherited Base instance
+        """
         filename = "{}.json".format(cls.__name__)
         with open(filename, 'w') as f:
             if list_objs is None:
@@ -38,3 +43,18 @@ class Base:
                 for i in list_objs:
                     list_dict.append(i.to_dictionary())
                 f.write(Base.to_json_string(list_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        From json to dictionary
+
+        Args:
+            json_string (str): the json string to be changed to dict
+        Return:
+            empty list if json_string is None or empty. Otherwise
+            a list representing json_string
+        """
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
