14e36d5db248aa5bfda96a66a51ab2afe9e79a1e
Mehdi Masmar <masmar507@gmail.com>
2023-02-15 14:10:02 -0800

    Temperatures #1

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,2 @@
+-- Shows the 3 cities with highest average tempreatures between July and August
+SELECT city, AVG(value) AS avg_temp WHERE month = 7 OR month = 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
