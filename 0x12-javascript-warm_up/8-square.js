8047523f8a71794c4a158e1d155690d89bcf05f0
Mehdi Masmar <masmar507@gmail.com>
2023-03-19 15:46:29 -0700

    Fixing code style

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,10 +4,10 @@ if (isNaN(size[2])) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size[2]; i++) {
-	  let row = '';
-	  for (let j = 0; j < size[2]; j++) {
-		  row += 'X';
-	  }
-	  console.log(row);
+    let row = '';
+    for (let j = 0; j < size[2]; j++) {
+      row += 'X';
+    }
+    console.log(row);
   }
 }
