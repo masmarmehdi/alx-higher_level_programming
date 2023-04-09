9b81f17b1fc4295ab403a6be2f923ae8c1493cf5
Mehdi Masmar <masmar507@gmail.com>
2023-04-08 21:50:21 -0700

    pycodestyle

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,9 +14,9 @@ if __name__ == '__main__':
     )
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-            .format(sys.argv[4])
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-    cursor.close()
-    db.close()
+                   .format(sys.argv[4])
+                   rows=cursor.fetchall()
+                   for row in rows:
+                       print(row)
+                   cursor.close()
+                   db.close()
