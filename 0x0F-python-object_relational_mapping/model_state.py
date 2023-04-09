56142400f46ff21ee0c072b4d721966f03970ce6
Mehdi Masmar <masmar507@gmail.com>
2023-04-08 22:50:43 -0700

    forgot /

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """Contains State and Base classes + declarative base function"""
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
